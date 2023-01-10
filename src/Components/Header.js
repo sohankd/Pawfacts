65ce8f8e9c4c7c94a331f6ecd32888ba7b572616
Sohan <sohankd5419@gmail.com>
2023-01-10 14:53:41 +0530

    Logo added and unnecessary files were removed

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,7 +2,8 @@
 function Header(props){
     return (
         <header className="header">
-            <a href="/" className="homepage">
+            <a href="/" className="header-logo-link">
+                <img className="header-logo" src="Dog_High_Five.svg" alt="Dog Logo" />
                 <span className="header-text">PAWFACTS</span>
             </a>
         </header>
